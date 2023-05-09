09f2dba30050d0169c1420079901f8345942f711
Fred Yang <fredyangify@gmail.com>
2023-05-09 00:31:41 -0400

    interface for Commands

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,15 +1,14 @@
 import * as fs from 'fs'
 import * as path from 'path'
-import { Client, Collection, Events, GatewayIntentBits } from 'discord.js'
+import {Client, Collection, Events, GatewayIntentBits} from 'discord.js'
 import * as secret from './secret/secret.json'
-
+import Command from "./interfaces/Command";
 
 // init Client
 const client = new Client({ intents: [GatewayIntentBits.Guilds] })
 
-
 // load commands
-client.commands = new Collection()
+const commands:Collection<string, Command> = new Collection()
 
 const commandsPath = path.join(__dirname, 'commands')
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'))
@@ -20,7 +19,7 @@ for (const file of commandFiles) {
 		.then(command => {
 			// Set a new item in the Collection with the key as the command name and the value as the exported module
 			if ('data' in command && 'execute' in command) {
-				client.commands.set(command.data.name, command)
+				commands.set(command.data.name, command)
 			} 
 			else {
 				console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
@@ -41,7 +40,7 @@ client.on(Events.InteractionCreate, async interaction => {
 		return
 	}
 
-	const command = interaction.client.commands.get(interaction.commandName)
+	const command = commands.get(interaction.commandName)
 
 	if (!command) {
 		console.error(`No command matching ${interaction.commandName} was found.`)
---
+++ app/src/interfaces/Command.ts
@@ -0,0 +1,8 @@
+import {ChatInputCommandInteraction, SlashCommandBuilder} from "discord.js";
+
+interface Command {
+    data: SlashCommandBuilder,
+    execute: (interaction: ChatInputCommandInteraction) => Promise<void>,
+}
+
+export default Command
