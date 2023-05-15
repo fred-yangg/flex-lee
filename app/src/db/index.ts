50633955b45d1dd6aff297c73f9a3896316e8e81
Fred Yang <fredyangify@gmail.com>
2023-05-15 19:39:51 -0400

    removed ssh tunneling

--- app/src/db/index.ts
+++ app/src/db/index.ts
@@ -1,12 +1,7 @@
 import { Pool } from "pg"
 import * as pgConfig from "../config/pg.json"
-import {exec} from "child_process";
 
 const pool = new Pool(pgConfig)
-
-if (pgConfig.use_ssh_tunnel) {
-    exec(`ssh -L ${pgConfig.port}:${pgConfig.tunnel_host}:${pgConfig.tunnel_port} ${pgConfig.ssh_user}@${pgConfig.ssh_host}`)
-}
 
 export default async function query(text: string, values?: any[]) {
     const start = Date.now()
